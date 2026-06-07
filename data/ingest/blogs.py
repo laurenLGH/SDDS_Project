32b61dedf3c1f5c6fcee08934a940478dc20de3b
Lauren Hoge <laurenhoge@Laurens-MacBook-Air-5.local>
2026-06-07 12:13:13 -0400

    Adapted to add more blogs

--- data/ingest/blogs.py
+++ data/ingest/blogs.py
@@ -5,22 +5,41 @@ from pathlib import Path
 from bs4 import BeautifulSoup
 
 
-#using RSS to scrape xml from blogs - not web scraping becuase blocked on some sites
+#using RSS to scrape xml from blogs - web scraping blocked on some sites
 
 DB_PATH = Path('data/corpus.db')
 
 #Setting this variable as number of days in past to look for blogs 
 # TODO take this setting out of individual files (set accross search)
 
-DAYS_BACK = 10 
+DAYS_BACK = 20
 
 #All blogs to check - Can add more here or remove
 BLOG_SOURCES = [
     {'name': 'Microsoft Security Blog', 'url': 'https://www.microsoft.com/en-us/security/blog/feed/'},
     {'name': 'Cisco Talos',             'url': 'https://blog.talosintelligence.com/rss'},
     {'name': 'Krebs on Security',       'url': 'https://krebsonsecurity.com/feed/'},
-    {'name': 'Google Project Zero',     'url': 'http://googleprojectzero.blogspot.com/feeds/posts/default'}
+    {'name': 'Google Project Zero',     'url': 'http://googleprojectzero.blogspot.com/feeds/posts/default'},
+    {'name': 'Bleeping Computer',       'url': 'https://www.bleepingcomputer.com/feed/'},
+    {'name': 'Dark Reading',            'url': 'https://www.darkreading.com/rss.xml'},
+    {'name': 'VulnCheck',               'url': 'https://www.vulncheck.com/blog/rss.xml'},
+    {'name': 'Google Bug Hunters',      'url': 'https://bughunters.google.com/blog/rss'},
+    {'name': 'CERT',                    'url': 'https://www.kb.cert.org/vulfeed'}
 ]
+
+def get_entry_date(entry):
+    """
+    Extract publication date from RSS or Atom feed entry.
+    """
+    for field in (
+        "published_parsed",
+        "updated_parsed",
+        "created_parsed",
+    ):
+        if field in entry and entry[field]:
+            return pd.Timestamp(*entry[field][:6])
+
+    return None
 
 
 def get_blogs(days_back=DAYS_BACK):
@@ -28,21 +47,28 @@ def get_blogs(days_back=DAYS_BACK):
     all_rows = []
 
     for source in BLOG_SOURCES:
+
         feed = feedparser.parse(source['url'])
 
         for blog in feed.entries:
-            pub_date = pd.Timestamp(*blog.published_parsed[:6])
+
+            pub_date = get_entry_date(blog)
             if pub_date < cutoff:
                 continue
 
-            raw_html   = blog.get('content', [{}])[0].get('value', '') or blog.get('summary', '')
-            clean_text = BeautifulSoup(raw_html, 'html.parser').get_text(separator=' ', strip=True)
+            raw_html = (
+                blog.get('content', [{}])[0].get('value', '')
+                or blog.get('summary', '')
+            )
+
+            clean_text = BeautifulSoup(raw_html,'html.parser').get_text(
+                separator=' ',strip=True)
 
             all_rows.append({
-                'source' : source['name'],
-                'date'   : str(pub_date.date()),
-                'title'  : blog.get('title', ''),
-                'url'    : blog.get('link', ''),
+                'source': source['name'],
+                'date': str(pub_date.date()),
+                'title': blog.get('title', ''),
+                'url': blog.get('link', ''),
                 'content': clean_text,
             })
 
